7e9964a6fb4588bae1b7b4be4edc0ebbb62576ff
SangboemPark <wjsvhfdkatks@naver.com>
2024-08-29 10:42:33 +0900

    get, main

--- shoes_app/lib/main.dart
+++ shoes_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoes_app/view/sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const SignIn(),
     );
   }
 }
